2f02b36e5428ad6b64ab386da80adcc93597ce2d
Brooke Brown <brooke.henriksson@outlook.com>
2025-03-26 09:42:48 +0100

    Completed Exercise 1 Random Number Guessing

---
+++ js/guess.js
@@ -0,0 +1,23 @@
+let input = prompt("Guess a number from 1 to 10:");
+
+//CALCULATE A RANDOM INTEGER
+let randNum = Math.floor(Math.random()*10)+1;
+
+
+while(input != randNum){
+
+
+    if(input > randNum){
+        alert(`Your guess was too high! The Random Number is: ${randNum}`);
+        input = prompt("Guess lower number from 1 to 10:");
+    }
+
+    if(input < randNum){
+        alert(`Your guess was too low! The Random Number is: ${randNum}`);
+        input = prompt("Guess higher number from 1 to 10:");
+    }
+
+
+}
+
+8
